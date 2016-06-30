1bf4c5015f9aa0039ed60409e4fbe84b0711c47f
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-06-30 22:36:00 +0200

    implement dumb comp @input/@output

--- src/app/questions/answer.component.ts
+++ src/app/questions/answer.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'app-answer',
   template: `
     <p>
-      answer works!
+      Q: <b>{{question}}?</b> <br />
+      R: <input #resp [value]="answer"  
+          (blur)="save(resp.value)"
+          (keyup.enter)="save(resp.value)" 
+          (keyup.esc)="resp.value=undo()" >
     </p>
   `,
   styles: []
 })
 export class AnswerComponent implements OnInit {
-  constructor() {}
+  @Input() question: string;
+  @Input() answer: string;
+  @Output() onResponse: EventEmitter<string>;
+  constructor() {
+    this.onResponse = new EventEmitter<string>();
+  }
 
   ngOnInit() {
+    this.answer = this.answer || '';
+  }
+
+  undo() {
+    return this.answer || '';
+  }
+
+  save(resp: string) {
+    if (resp !== this.answer) {
+      this.onResponse.emit(resp);
+    }
   }
 
 }
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionstoreService, Question } from './index';
+import { AnswerComponent } from './answer.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-questions',
   templateUrl: 'questions.component.html',
   styleUrls: ['questions.component.css'],
-  providers: [ QuestionstoreService ]
+  providers: [ QuestionstoreService ],
+  directives: [ AnswerComponent ]
 })
 export class QuestionsComponent implements OnInit {
   questions: Question[];
@@ -14,6 +16,10 @@ export class QuestionsComponent implements OnInit {
 
   ngOnInit() {
     this.questions = this.svc.getAll();
+  }
+
+  setResponse(idx: number, resp: string) {
+    this.questions[idx].res = resp;
   }
 
 }
