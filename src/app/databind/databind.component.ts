1869aea7d3eed3677105a0444cff282046798421
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-06-30 19:00:24 +0200

    added .box class to show css incapsulation

--- src/app/databind/databind.component.ts
+++ src/app/databind/databind.component.ts
@@ -4,13 +4,16 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'app-databind',
   template: `
+  <div class="box">
     What is your name? <input [(ngModel)]="name">
     <p *ngIf="name.length>0">
       Hi, {{name}}! <button (click)="clear()">X</button>
     </p>
+  </div>
   `,
   styles: [
-    'button { color: red; border: 1px solid }'
+    'button { color: red; border: 1px solid }',
+    '.box { border: 2px solid red; margin: 5px }'
   ]
 })
 export class DatabindComponent implements OnInit {
