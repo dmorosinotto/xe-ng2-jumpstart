42408eb78430a97fd95c7926c362c8ffbe904987
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-06-30 18:32:46 +0200

    ng g component -it -is databind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { DatabindComponent } from './databind';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [DatabindComponent]
 })
 export class AppComponent {
   title = 'NG2 JumpStart Confartigianato!';
---
+++ src/app/databind/databind.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-databind',
+  template: `
+    <p>
+      databind Works!
+    </p>
+  `,
+  styles: []
+})
+export class DatabindComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
